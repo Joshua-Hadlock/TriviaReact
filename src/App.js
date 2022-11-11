549949a8876717d171d74c90b138a72916ea69c9
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro.local>
2022-11-11 09:45:48 -0700

    title page started

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './css/App.css';
 
-
-
-
-function bottomNavBar() {
-  return (
-    <div className='bottomNav'>
-      <button>Home</button>
-      <button>About</button>
-      <button>Lyrics</button>
-      <button>Game</button>
-    </div>
-  )
-}
 function App() {
   return (
-    <>
-    <div className='body'>
-      <h1 className='title'>MCQUACKERS LYRIC FINDER</h1>
+    <div>
+      <div className="main">
+        <div className="mainText">
+          <h1>Test your knowledge with 
+            Mcquackers
+          </h1>
+        </div>
+        <div class="triviaIcons"></div>
+      </div>
     </div>
-      <div>hello there</div>
-      {bottomNavBar()}
-    </>
   );
 }
 
