9e59f60933df4a0657283a2318c185cb472539ba
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro.local>
2022-11-14 10:18:20 -0700

    Main Page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ function App() {
   return (
     <div>
       <div className="main">
+        <div className="logo"></div>
         <div className="mainText">
           <h1>Test your knowledge with 
-            Mcquackers
+            McQuackers
           </h1>
+          <button>Click here to get started</button>
         </div>
-        <div class="triviaIcons"></div>
+        <div className="triviaIcons"></div>
       </div>
+      <div className="navBar">
+        <div className="navContent">
+          <ul>
+          <li>Home</li>
+          <li>Help</li>
+          <li>About</li>
+          <li>Trivia!</li>
+        </ul>
+        </div>
+        
+      </div>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
