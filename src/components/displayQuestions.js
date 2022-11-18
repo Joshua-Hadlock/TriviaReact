d1b098853032d0b77f9467b2c4f9f7efa59d7792
Joshua Hadlock <jhadlock@Wades-MacBook-Pro.local>
2022-11-18 10:23:37 -0700

    more trivia functions

--- src/components/displayQuestions.js
+++ src/components/displayQuestions.js
@@ -3,20 +3,35 @@ import { useState, useEffect } from "react";
 
 
 export default function DisplayQuestions(data) {
+    const [questionsAndAnswers, setQestionsAndAnswers] = useState();
+    const [score, setScore] = useState(0);
     useEffect(() => {
             document.getElementById('scene-container').innerHTML = '';
             displayCube(document.getElementById('scene-container'));
-            console.log('running')
-            
-            
-            
-        
+            console.log('running');
     }, []);
+
+    const handleClickIncorrect = event => {
+        event.currentTarget.classList.add('bg-red');
+    }
+
+    const handleClickCorrect = event => {
+        event.currentTarget.classList.add('bg-green');
+        const newScore = score + 1;
+        setScore(newScore);
+        console.log(score);
+    }
+
+
     if (data) {
         if (document.getElementById('scene-container')) {
             document.getElementById('scene-container').innerHTML = '';
         }
+
+
+        
         console.log(data.results)
+        
         const displayableData = data.results.map((value) => {return (
         <div className="triviaCard">
             <div>
@@ -24,9 +39,9 @@ export default function DisplayQuestions(data) {
                 <div className="question"></div>
             </div>
             <div>
-                <h5 className="textWhite">{value.correct_answer}</h5>
+                <h5 className="textWhite answer" onClick={handleClickCorrect}>{value.correct_answer}</h5>
                 {value.incorrect_answers.map((incorrectAnswer) => {
-                    return (<h5 className="textWhite">{incorrectAnswer}</h5>)
+                    return (<h5 className="textWhite answer" onClick={handleClickIncorrect}>{incorrectAnswer}</h5>)
                 })}
 
                 
