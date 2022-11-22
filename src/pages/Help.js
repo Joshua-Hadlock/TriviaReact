5c892860161196be0fb1f568f9b14c23c6d68d2b
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro.local>
2022-11-22 07:57:30 -0700

    Pulling from Josh

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -12,7 +12,7 @@ export default function HelpPage() {
       var value = window.scrollY;
 
       bg.style.top = value + 'px';
-      moon.style.left = -value * 0.5 + 'px';
+      moon.style.left = -value * 0.5  + 'px';
       mountain.style.top = -value * 0.15 + 'px';
       road.style.top = value * 0.15 + 'px';
       text.style.top = value * 1 + 'px';
