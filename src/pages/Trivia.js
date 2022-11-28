83c3b287ecfa7eb4555e6e4880d5baea096f6641
Isaac Tellez <isaactellez@Isaacs-MacBook-Pro.local>
2022-11-28 09:01:32 -0700

    random changes

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -1,12 +1,11 @@
 import getApi from "../components/triviaAPI";
 import { useState, useEffect } from "react";
 import DisplayQuestions from "../components/displayQuestions";
-import createData from "../components/createData";import { linkClasses } from "@mui/material";
-;
+import createData from "../components/createData";
 
 
 
-const tryApiData = {"response_code":0,"results":[{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"Which company did Valve cooperate with in the creation of the Vive?","correct_answer":"HTC","incorrect_answers":["Oculus","Google","Razer"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"On a dartboard, what number is directly opposite No. 1?","correct_answer":"19","incorrect_answers":["20","12","15"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"What is the French word for &quot;hat&quot;?","correct_answer":"Chapeau","incorrect_answers":["Bonnet"," &Eacute;charpe"," Casque"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"Which sign of the zodiac comes between Virgo and Scorpio?","correct_answer":"Libra","incorrect_answers":["Gemini","Taurus","Capricorn"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"In which fast food chain can you order a Jamocha Shake?","correct_answer":"Arby&#039;s","incorrect_answers":["McDonald&#039;s","Burger King","Wendy&#039;s"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"What zodiac sign is represented by a pair of scales?","correct_answer":"Libra","incorrect_answers":["Aries","Capricorn","Sagittarius"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"When someone is inexperienced they are said to be what color?","correct_answer":"Green","incorrect_answers":["Red","Blue","Yellow"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"Foie gras is a French delicacy typically made from what part of a duck or goose?","correct_answer":"Liver","incorrect_answers":["Heart","Stomach","Intestines"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"Who is the author of Jurrasic Park?","correct_answer":"Michael Crichton","incorrect_answers":["Peter Benchley","Chuck Paluhniuk","Irvine Welsh"]},{"category":"General Knowledge","type":"multiple","difficulty":"easy","question":"Who is the youngest person to recieve a Nobel Prize?","correct_answer":"Malala Yousafzai","incorrect_answers":["Lawrence Bragg","Werner Heisenberg","Yasser Arafat"]}]}
+
 export default function TriviaPage() {
     const [category, setCategory] = useState("9");
     const [numberOfQuestions, setNumberOfQuestions] = useState(10);
@@ -96,9 +95,9 @@ export default function TriviaPage() {
             <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
             <option value="32">Entertainment: Cartoon &amp; Animations</option>		
       </select>
-    <input type={'number'} max={50} min={1} onChange={e => changeNumberOfQuestions(e.target.value)}></input>
+    <input className="numberOfTrivia" type={'number'} max={50} min={1} onChange={e => changeNumberOfQuestions(e.target.value)}></input>
 
-      <select name="difficulty" onChange={e => changeDifficulty(e.target.value)}>
+      <select className="difficultyDrop" name="difficulty" onChange={e => changeDifficulty(e.target.value)}>
 			{/* <option value="any">Any Difficulty</option> */}
 			<option value="easy">Easy</option>
 			<option value="medium">Medium</option>
@@ -111,7 +110,6 @@ export default function TriviaPage() {
         {DisplayQuestions(apiData)}
 
         <h1 className="off" id='Timer'>Timer: {count}</h1>
-        <div>padding</div>
       </div>
     )
   }
